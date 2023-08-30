326b21a8c853769d96ffa1eaca51285406e387d9
Arshad Ali <se.arshadali@gmail.com>
2023-08-30 17:27:41 +0400

    Commint my work
    first time.

---
+++ Frontend/src/app/app.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { PropertyCardComponent } from './Property/property-car/property-card.component';
+import { PropertyListComponent } from './Property/property-list/property-list.component';
+import { NavBarComponent } from './Property/nav-bar/nav-bar.component';
+
+@NgModule({
+  declarations: [AppComponent, PropertyCardComponent, PropertyListComponent, NavBarComponent],
+  imports: [BrowserModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
